ec467a46273c38a3e566ed58b51758113b392254
Cami <camilacammisa.01@gmail.com>
2025-08-20 15:07:10 -0300

    semana1 y semana2 terminadas

--- guia/semana1/main.cpp
+++ guia/semana1/main.cpp
@@ -1,4 +1,4 @@
-#include <iostream>
+ #include <iostream>
 #include <iomanip>
 
 using namespace std;
@@ -8,40 +8,102 @@ using namespace std;
 // Implementar aquí para usarlas más abajo.
 // -----------------------------------------------------------------------------
 void swap_por_puntero(int* a, int* b) {
-    // TODO: implementar intercambio clásico usando *a, *b y una variable temporal.
-    // int tmp = *a;
-    // *a = *b;
-    // *b = tmp;
+    // implementar intercambio clásico usando *a, *b y una variable temporal.
+    int tmp = *a;
+    *a = *b;
+    *b = tmp;
 }
 
 void swap_si_mayor(int* p, int* q) {
-    // TODO: intercambiar solo si *p > *q.
+    // intercambiar solo si *p > *q.
+    if(*p > *q){
+        int tmp= *p;
+        *p= *q;
+        *q= tmp; 
+    }
 }
 
 void swap_si_menor(int* p, int* q) {
-    // TODO (opcional): intercambiar solo si *p < *q.
+    // intercambiar solo si *p < *q.
+    if(*p < *q){
+        int tmp= *p;
+        *p= *q;
+        *q= tmp; 
+    }
 }
 
 void set_si_positivo(int* p, int valor) {
-    // TODO (opcional): escribir 'valor' en *p solo si valor > 0.
+    // escribir 'valor' en *p solo si valor > 0.
+    if(*p>0){
+        *p= valor;
+    }
 }
 
 // -----------------------------------------------------------------------------
 // (D) Funciones que procesan arreglos pasados por puntero
 // -----------------------------------------------------------------------------
 int suma(const int* arr, int n) {
-    // TODO: recorrer arr[0..n-1] y acumular.
-    return 0;
+    // recorrer arr[0..n-1] y acumular.
+    int suma=0;
+    for(int i=0; i<n; i++){
+        suma+=arr[i];
+    }
+    return suma;
 }
 
 double promedio(const int* arr, int n) {
-    // TODO: usar suma(...) y devolver (double)suma/n si n>0; si n==0 retornar 0.0.
+    //usar suma(...) y devolver (double)suma/n si n>0; si n==0 retornar 0.0.
+    //veo si n==0 y en caso de que si devuelvo 0.0
+    if(n==0)
     return 0.0;
+    //si no se retornó devuelvo el promedio
+    return suma(arr,n)/n;
+
 }
 
 int maximo(const int* arr, int n) {
-    // TODO (opcional): devolver el mayor valor del arreglo (validar n>0).
+    // devolver el mayor valor del arreglo (validar n>0).
+    //valido que n>0, en caso de que no retorno 0
+    if(n==0)
     return 0;
+    //encuentro el mayor valor del arreglo y lo devuelvo 
+    int max= arr[0];
+    for(int i=0; i<n; i++){
+        if(max<arr[i]){
+            max=arr[i];
+        }     
+    }
+    return max;
+}
+// -----------------------------------------------------------------------------
+// (C) Funciones 
+// -----------------------------------------------------------------------------
+void swap_por_puntero2(int &x, int&y){
+    int temp= x; 
+    x=y; 
+    y=temp;     
+}
+
+void swap_si_mayor2(int &x, int &y)
+{
+    if(x > y)
+    {
+        swap_por_puntero2(x,y); 
+    }
+}
+
+void swap_si_menor2(int &x, int &y)
+{
+    if(x<y)
+    swap_por_puntero2(x,y);
+
+}
+
+void set_si_positivo2(int &x, int valor){
+    if (valor>0)
+    {
+        x= valor; 
+    }
 }
 
 int main() {
@@ -59,6 +121,22 @@ int main() {
     //    cout << "c=" << c << "  &c=" << static_cast<const void*>(&c) << "\n";
     // 3) Escribir en comentarios 2-3 observaciones sobre el patrón de direcciones.
 
+    cout << "APARTADO A \n"; 
+    int a=5;
+    float f=3;
+    char c='Z';
+
+    cout << a << " " << &a << "\n";
+    cout << f << " " << &f << "\n";
+    cout << c << " " << static_cast<const void*>(&c) << "\n"; 
+
+    /*al imprimirlo la consola mostró:
+        5 0x5ffe8c
+        3.00 0x5ffe88
+        Z 0x5ffe87 */
+    // entre el int y el float hay una diferencia de 5 bytes entre una direccion y la otra(debido a que el float ocupa 4 bytes)
+    //en el byte restante se guardo el char lo que significa que ocupa 1 byte.
+
     // -------------------------------------------------------------------------
     // (B) Proyecto CasaDomotica++ - Parte 1: Sensor de temperatura por puntero
     // -------------------------------------------------------------------------
@@ -70,24 +148,76 @@ int main() {
     // 4) Modificar el valor a través del puntero: *ptrTemp = 27; y verificar.
     // 5) Leer una nueva temperatura desde consola y asignarla vía *ptrTemp.
 
+    cout << "APARTADO B \n"; 
+
+    // 1)
+    int temperaturaActual= 22; 
+    // 2)
+    int* ptrTemp= &temperaturaActual; 
+    // 3)
+    cout << "contenido en ptrTemp: " << *ptrTemp << "\n"<<  " contenido en temperaturaActual: " << temperaturaActual << "\n";
+    // 4)
+    *ptrTemp= 27; 
+    cout << "contenido en ptrTemp: " << *ptrTemp << "\n"<<  " contenido en temperaturaActual: " << temperaturaActual << "\n";
+    // 5)
+    cin >> *ptrTemp; 
+    cout << "contenido en ptrTemp: " << *ptrTemp << "\n"<<  " contenido en temperaturaActual: " << temperaturaActual << "\n"; 
+
+
     // -------------------------------------------------------------------------
     // (C) Paso por puntero: pruebas de swap y variantes
     // -------------------------------------------------------------------------
-    // TODO:
+     cout << "APARTADO C \n"; 
     // 1) Implementar swap_por_puntero arriba y probar con:
     //    int x=5, y=9;  swap_por_puntero(&x,&y);
+    int x=5; 
+    int y=9; 
+    cout << x << " " <<y << "\n"; 
+    swap_por_puntero2(x,y); 
+    cout << x << " " <<y << "\n";
+
     // 2) Implementar swap_si_mayor y probar casos (p.ej., 10 y 3, luego 2 y 7).
+    int z=10; 
+    int j=3; 
+    cout << z << " " << j << "\n";
+    swap_si_mayor2(z,j);
+    cout << z << " " << j << "\n";
+
+    int b= 2; 
+    int d= 7; 
+    cout << b << " " << d << "\n";
+    swap_si_mayor2(b,d);
+    cout << b << " " << d << "\n";
+
     // 3) (Opcional) swap_si_menor y set_si_positivo.
+    set_si_positivo2(b,5); 
+    cout << b << "\n"; 
+    set_si_positivo2(b,-1);
+    cout << b << "\n";
+
 
     // -------------------------------------------------------------------------
     // (D) Arreglos estáticos: direcciones y funciones con punteros
     // -------------------------------------------------------------------------
     // 1) Declarar un arreglo fijo: int v[5] = {10,20,30,40,50};
+    int v[5]={10,20,30,40,50};
     // 2) Imprimir v[i] y &v[i] en un for (i=0..4) para observar contigüidad.
     //    Sugerencia de línea para dirección:
     //    cout << (void*)(&v[i]) << "\n";
+    for(int i=0; i<5; i++)
+    {
+        cout << "direccion: " << (void*)&v[i] << " " <<  "contenido: " << v[i] << "\n";
+
+    }
+
     // 3) Llamar a suma(v,5), promedio(v,5) y (opcional) maximo(v,5) e imprimir.
-    //    // TODO: completar implementaciones arriba.
+   int sum= suma(v,5);
+   double prom= promedio(v,5);
+   int max= maximo(v,5);
+
+   cout << sum << " " << prom << " " << " " << max; 
+   
     cout << "\nFin Semana 1. Próximo paso: memoria dinámica y archivos (Semana 2).\n";
     return 0;
+
 }
--- guia/semana2/src/readings.cpp
+++ guia/semana2/src/readings.cpp
@@ -12,17 +12,43 @@ float* crear_arreglo(int size) {
 }
 
 void liberar_arreglo(float*& arr) {
-    //TODO
+    delete[]arr; 
+    arr= nullptr; 
 }
 
 int cargar_desde_archivo(const std::string& ruta, float* arr, int size) {
     //TODO
+    std::ifstream archivo(ruta); 
+    if(!archivo){
+        std::cout << "No se pudo abrir el archivo";
+        return 1; 
+    }
+    int cant_leidos=0; 
+    for(int i=0; i<size; i++)
+    {
+        archivo >> arr[i];
+        cant_leidos++;  
+    }
+    archivo.close(); 
+    return cant_leidos; 
 }
 
 bool guardar_en_archivo(const std::string& ruta, const float* arr, int size) {
-    // TODO
+    std::ofstream salida(ruta); 
+    if(!salida){
+        std::cout << "No se pudo abrir el archivo para escribir"; 
+        return 0; 
+    }
+    for(int i=0; i<size; i++){
+        salida << arr[i] << std::endl; 
+    }
+    salida.close();
+    return 1; 
 }
 
 void imprimir(const float* arr, int size) {
-    // TODO
+    for(int i=0; i<size; i++){
+        std::cout << arr[i]<< "\n"; 
+    }
 }
+ 
