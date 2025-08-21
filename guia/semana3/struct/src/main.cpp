dc040ecb6fbd318df4f2b117f6097d3e28ebce66
Cammi <camilacammisaingenieria@gmail.com>
2025-08-21 17:41:20 -0300

    Update main.cpp

--- guia/semana3/struct/src/main.cpp
+++ guia/semana3/struct/src/main.cpp
@@ -2,24 +2,27 @@
 #include "sensor.h"
 
 using namespace std;
-
-int main() {
+nt main() {
     
-    // int N;
-    // cout << "Cantidad de sensores: ";
-    // cin >> N;
+    int N;
+    cout << "Cantidad de sensores: ";
+    cin >> N;
 
     //TODO: Instanciar un array de Sensores de forma dinamica y de tamaño N
+    Sensor* sensores= new Sensor[N];  
+    
+    
+    string archivoEntrada;
+    cout << "Archivo de entrada: ";
+    cin >> archivoEntrada;
 
-    // string archivoEntrada;
-    // cout << "Archivo de entrada: ";
-    // cin >> archivoEntrada;
+    int leidos = cargarDesdeArchivo(archivoEntrada, sensores, N);
+    cout << "Se leyeron " << leidos << " sensores.\n";
 
-    // int leidos = cargarDesdeArchivo(archivoEntrada, sensores, N);
-    // cout << "Se leyeron " << leidos << " sensores.\n";
-
-    // imprimir(sensores, leidos);
+    imprimir(sensores, leidos);
 
     // TODO: Que habria que hacer aca??
+    free.()
     return 0;
 }
+
