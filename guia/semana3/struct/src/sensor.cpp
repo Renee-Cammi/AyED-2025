624b486f87842f21b7c756eb1adeff2ed011b4bf
Cammi <camilacammisaingenieria@gmail.com>
2025-08-21 17:39:42 -0300

    Update sensor.cpp

--- guia/semana3/struct/src/sensor.cpp
+++ guia/semana3/struct/src/sensor.cpp
@@ -4,9 +4,28 @@
 #include <iomanip>
 
 void imprimir(const Sensor* lista, int n) {
-
+	/*for(int i=0; i<n; i++){
+		cout << lista[i].nombre<< " " << lista[i].valor<< " "<< lista[i].unidad << endl;
+		} */
+		cout << (lista+i)->nombre << (lista+i)->valor << (lista+i)->unidad << endl;
 }
 
 int cargarDesdeArchivo(const std::string& ruta, Sensor* lista, int n) {
-
+	ifstream archivo(ruta);
+	if(!archivo){
+		cout <<"no se pudo abrir el archivo";
+		return 0;
+		}
+		int leidos=0;
+	/*for(int i=0; i<n;i++){
+		archivo >> lista[i].nombre; 
+		archivo >> lista[i].valor;
+		archivo >> lista[i].unidad;
+		leidos++;
+		}*/	
+		
+	while (leidos<n && (archivo >>lista[leidos].nombre >> lista[leidos].valor >>lista[leidos].unidad)){
+		leidos ++;
+		}
+		return leidos; 
 }
