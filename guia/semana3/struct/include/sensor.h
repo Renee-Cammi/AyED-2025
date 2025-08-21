4450bae9fdf4d96e28e526faf354bf6e4a0e4978
Cammi <camilacammisaingenieria@gmail.com>
2025-08-21 17:41:52 -0300

    Update sensor.h

--- guia/semana3/struct/include/sensor.h
+++ guia/semana3/struct/include/sensor.h
@@ -4,7 +4,11 @@
 #include <string>
 
 // Declarar un struct Sensor que tenga un nombre (string), valor (flotante) y unidad (string
-
+struct Sensor{
+	string nombre;
+	float valor;
+	string unidad;
+	}
 // Prototipos
 void imprimir(const Sensor* lista, int n);
 int cargarDesdeArchivo(const std::string& ruta, Sensor* lista, int n);
